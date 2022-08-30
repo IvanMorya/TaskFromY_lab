9f75151b397e55bb08bac55ef69be32181b3e563
moryi <mory.ivan@gmail.com>
2022-08-30 13:57:03 +0300

    Add Task 1.2

---
+++ src/main/java/task1/Task12.java
@@ -0,0 +1,39 @@
+package task1;
+
+public class Task12 {
+    static int partition(int[] array, int begin, int end) {
+        int pivot = end;
+
+        int counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (array[i] < array[pivot]) {
+                int temp = array[counter];
+                array[counter] = array[i];
+                array[i] = temp;
+                counter++;
+            }
+        }
+        int temp = array[pivot];
+        array[pivot] = array[counter];
+        array[counter] = temp;
+
+        return counter;
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(array, begin, end);
+        quickSort(array, begin, pivot-1);
+        quickSort(array, pivot+1, end);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 6, 3, 2, 5, 1, 4, 9};
+
+        quickSort(array, 0, array.length - 1);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
